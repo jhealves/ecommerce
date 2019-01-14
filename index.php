a757b33365086adc5c4a49ea8f84ab22703a842d
Jeberson Alves <jephalves@gmail.com>
2019-01-13 23:08:45 -0200

    erro na aula 106

--- index.php
+++ index.php
@@ -30,20 +30,83 @@ $app->get('/admin', function() {
 
 $app->get('/admin/login', function(){
 	$page = new PageAdmin([
+
 		"header"=>false,
 		"footer"=>false
 	]);
+
 	$page->setTpl("login");
+
 });
+
 $app->post('/admin/login',function(){
+
 	User::login($_POST["login"], $_POST["password"]);
+
 	header("Location: /admin");
+
 	exit;
 });
+
 $app->get('/admin/logout', function(){
+
 	User::logout();
+
 	header("Location: /admin/login");
 	exit;
+
+});
+$app->get("/admin/users", function(){
+
+	User::verifyLogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+
+		"users"=>$users
+
+	));
+});
+
+$app->get("/admin/users/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+
+});
+
+$app->get("/admin/users/:iduser/delete", function($iduser){
+
+	User::verifyLogin();
+
+});
+
+$app->get("/admin/users/:iduser", function($iduser){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update");
+
+});
+
+$app->post("/admin/users/create", function(){
+
+	User::verifyLogin();
+
+});
+
+$app->post("/admin/users/:iduser", function($iduser){
+
+	User::verifyLogin();
+
 });
 
 $app->run();
